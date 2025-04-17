9baa2434c91be5e3e27b987ef672e111eac073b1
BelovaD <godfire319@gmail.com>
2025-04-18 00:31:27 +0500

    solo_update_code

--- Solo.php
+++ Solo.php
@@ -1,5 +1,47 @@
 <?php
 
+class GameSettings
+{
+    private static $instance;
 
+    private $settings;
+
+    private function __construct()
+    {
+        $this->settings = [
+            'fullscreen' => false,
+            'volume' => 50,
+            'difficulty' => 'easy',
+        ];
+    }
+
+    public static function getInstance(): GameSettings
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getSettings(): array
+    {
+        return $this->settings;
+    }
+
+    public function setSetting(string $key, $value): void
+    {
+        if (array_key_exists($key, $this->settings)) {
+            $this->settings[$key] = $value;
+        }
+    }
+}
+
+$gameSettings = GameSettings::getInstance();
+print_r($gameSettings->getSettings());
+
+$gameSettings->setSetting('fullscreen', true);
+$gameSettings->setSetting('volume', 70);
+$gameSettings->setSetting('difficulty', 'hard');
+print_r($gameSettings->getSettings());
 
 ?>
