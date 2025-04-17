92d907e4915921a691cfe4050ace8a0403df597d
BelovaD <godfire319@gmail.com>
2025-04-17 23:28:19 +0500

    prototype_update

--- Prototype.php
+++ Prototype.php
@@ -3,21 +3,30 @@
 interface orderManagementSystem
 {
     public function clone();
+
 }
 
 class Order implements orderManagementSystem {
-    private $id;
-    public function __construct($id) {
-        $this->id = $id;
+
+    private $orderNumber;
+    private $items;
+    private $totalAmount;
+    public function __construct(string $orderNumber, string $items, float $totalAmount) {
+        $this->orderNumber = $orderNumber;
+        $this->items = $items;
+        $this->totalAmount = $totalAmount;
     }
     public function clone(){
-        return new Order($this->id);
+        return new Order($this->orderNumber, $this->items, $this->totalAmount);
+    }
+    public function __tostring(): string {
+        
+        return "Order №({$this->orderNumber}, {$this->items}, {$this->totalAmount})";
     }
 }
 
-$a=new Order(5);
+$a=new Order(2,'tomatoes, cucumbers, milk, meat, fish',1300);
 $b= $a->clone()
-
 
 ?>
 
